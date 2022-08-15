7561b8928d119089c05c38a9a434bc3414451fd9
Hamed Osama <hamedosama19@gmail.com>
2022-08-15 22:55:30 +0200

    fname input validation added

--- Forms/src/components/BasicForm.js
+++ Forms/src/components/BasicForm.js
@@ -1,10 +1,30 @@
+import React from 'react'
+
+import useInput from '../hooks/use-input'
 const BasicForm = (props) => {
+
+  const {
+    value: enteredFname,
+    isValid: enteredFnameIsValid,
+    hasError: FnameHasError,
+    onChangeHandler: onChangeFnameHandler,
+    onBlurHandler: onBlurFnameHandler,
+    resetInputHandler: resetFnameInput
+  } = useInput(value => value.trim() !== '')
+  const fNameClasses = !FnameHasError ? 'form-control' : 'form-control invalid'
   return (
     <form>
       <div className='control-group'>
-        <div className='form-control'>
+        <div className={fNameClasses}>
           <label htmlFor='name'>First Name</label>
-          <input type='text' id='name' />
+          <input
+            type='text'
+            id='name'
+            value={enteredFname}
+            onChange={onChangeFnameHandler}
+            onBlur={onBlurFnameHandler}
+          />
+          {FnameHasError && <p>First name Must Not Be Empty</p>}
         </div>
         <div className='form-control'>
           <label htmlFor='name'>Last Name</label>
