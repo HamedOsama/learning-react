70dfae6fe84a31f166c66186ab499ce8749c7fac
Hamed Osama <hamedosama19@gmail.com>
2022-08-23 01:04:31 +0200

    initialize app

--- redux/src/App.js
+++ redux/src/App.js
@@ -1,10 +1,9 @@
 import { Fragment } from 'react';
 import { useSelector } from 'react-redux';
 
-
-import Header from './components/Header'
-import Auth from './components/Auth'
-import UserProfile from './components/UserProfile'
+import Header from './components/Header';
+import Auth from './components/Auth';
+import UserProfile from './components/UserProfile';
 import Counter from './components/Counter';
 
 function App() {
