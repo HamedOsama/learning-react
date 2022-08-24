4f0bba2541a37894528efc1a24c0406485ab644b
Hamed Osama <hamedosama19@gmail.com>
2022-08-24 22:16:45 +0200

    comments route added

--- pratice-react-router/src/pages/QuoteDetail.js
+++ pratice-react-router/src/pages/QuoteDetail.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { useParams, Route } from 'react-router-dom'
+import { useParams, Route, Link } from 'react-router-dom'
 
 import HighlightedQuotes from '../components/quotes/HighlightedQuote'
 import Comments from '../components/comments/Comments'
@@ -19,6 +19,11 @@ function QuoteDetail() {
   return (
     <div>
       <HighlightedQuotes text={quote.text} author={quote.author} />
+      <Route path={`/quotes/${quoteId}`} exact>
+        <div className='centered'>
+          <Link className='btn--flat' to={`/quotes/${quoteId}/comments`}>Load Comments</Link>
+        </div>
+      </Route>
       <Route path={`/quotes/${quoteId}/comments`}>
         <Comments />
       </Route>
