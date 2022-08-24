ef86240a87f64f1ecde92b3b89292b4d1de02307
Hamed Osama <hamedosama19@gmail.com>
2022-08-24 07:36:29 +0200

    redirect to quotes page after add new quote

--- pratice-react-router/src/pages/NewQuote.js
+++ pratice-react-router/src/pages/NewQuote.js
@@ -1,10 +1,12 @@
 import React from 'react'
-
+import { useHistory } from 'react-router-dom'
 import QuoteForm from '../components/quotes/QuoteForm'
 
 function NewQuote() {
+  const history = useHistory();
   const addQuoteHandler = QuoteData => {
-    console.log(QuoteData)
+    console.log(QuoteData);
+    history.push('/quotes');
   }
   return <QuoteForm onAddQuote={addQuoteHandler} />
 }
