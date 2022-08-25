c4637c34ec1fae62977773fe9340e6dcbc6a0fb0
Hamed Osama <hamedosama19@gmail.com>
2022-08-26 01:45:04 +0200

    navigation guardian added

--- react-auth/src/App.js
+++ react-auth/src/App.js
@@ -1,4 +1,5 @@
-import { Switch, Route } from 'react-router-dom';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { useSelector } from 'react-redux';
 
 import Layout from './components/Layout/Layout';
 import UserProfile from './components/Profile/UserProfile';
@@ -6,17 +7,25 @@ import AuthPage from './pages/AuthPage';
 import HomePage from './pages/HomePage';
 
 function App() {
+  const isLoggedIn = useSelector(state => state.auth.isLoggedIn)
   return (
     <Layout>
       <Switch>
         <Route path='/' exact>
           <HomePage />
         </Route>
-        <Route path='/auth'>
-          <AuthPage />
-        </Route>
+        {
+          !isLoggedIn &&
+          <Route path='/auth'>
+            <AuthPage />
+          </Route>
+        }
         <Route path='/profile'>
-          <UserProfile />
+          {isLoggedIn && <UserProfile />}
+          {!isLoggedIn && <Redirect to='/auth' />}
+        </Route>
+        <Route path="*">
+          <Redirect to="/" />
         </Route>
       </Switch>
     </Layout>
@@ -24,3 +33,4 @@ function App() {
 }
 
 export default App;
+// navigation guardian added
