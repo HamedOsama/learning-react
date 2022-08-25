5a533a573fc4484c45e2a21c72798a9dd20b2434
Hamed Osama <hamedosama19@gmail.com>
2022-08-25 23:00:05 +0200

    signup and loggin added

--- react-auth/src/components/Auth/AuthForm.js
+++ react-auth/src/components/Auth/AuthForm.js
@@ -1,28 +1,92 @@
-import { useState } from 'react';
+import { useState, useRef } from 'react';
 
 import classes from './AuthForm.module.css';
-
 const AuthForm = () => {
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
+  const [isLoading, setIsLoading] = useState(false);
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
+  const signupHandler = async (email, password) => {
+    setIsLoading(true);
+    const req = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDEyeUpB0IDtUIjg4Iy8qcgPFCMUPvX0jY', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    setIsLoading(false);
+    const resData = await req.json();
+    console.log(resData)
+    if (!req.ok) {
+      let errorMessage = 'Signup failed!';
+      if (resData && resData.error && resData.error.message) {
+        errorMessage = resData.error.message.toLowerCase();
+      }
+      alert(errorMessage);
+    }
+  }
+  const loginHandler = async (email, password) => {
+    const req = await fetch('https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyDEyeUpB0IDtUIjg4Iy8qcgPFCMUPvX0jY', {
+      method: 'POST',
+      body: JSON.stringify({
+        email,
+        password,
+        returnSecureToken: true
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    })
+    const resData = await req.json();
+    console.log(resData);
+    if (!resData.error)
+      return resData;
+    else {
+      let errorMessage = 'Login failed!';
+      if (resData && resData.error && resData.error.message) {
+        errorMessage = resData.error.message.toLowerCase();
+      }
+      alert(errorMessage);
+    }
+  }
+  const submitHandler = (e) => {
+    e.preventDefault();
+
+    const email = emailInputRef.current.value;
+    const password = passwordInputRef.current.value;
+
+    if (isLogin) {
+      loginHandler(email, password);
+    }
+    else {
+      signupHandler(email, password);
+    }
+  }
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
       <form>
         <div className={classes.control}>
           <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <input ref={emailInputRef} type='email' id='email' required />
         </div>
         <div className={classes.control}>
           <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <input ref={passwordInputRef} type='password' id='password' required />
         </div>
         <div className={classes.actions}>
-          <button>{isLogin ? 'Login' : 'Create Account'}</button>
+          {!isLoading && <button onClick={submitHandler}>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <button disabled={true}>loading...</button>}
           <button
             type='button'
             className={classes.toggle}
--- react-auth/src/index.js
+++ react-auth/src/index.js
@@ -3,10 +3,10 @@ import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
-
 ReactDOM.render(
   <BrowserRouter>
     <App />
-  </BrowserRouter>,
+  </BrowserRouter>
+  ,
   document.getElementById('root')
 );
